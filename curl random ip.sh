8021a394427cd679d9051ffd1d8fc17786e9647a
lucalann <59168085+lucalann@users.noreply.github.com>
2020-07-15 10:51:27 +0200

    Update curl random ip.sh

--- curl random ip.sh
+++ curl random ip.sh
@@ -1,2 +1,2 @@
 while true; do curl curl --header "X-Forwarded-For: $(dd if=/dev/urandom bs=4 count=1 2>/dev/null | od -An -tu1 | sed -e 's/^ 
-*//' -e 's/  */./g')" http://istio-ingressgateway-istio-system-test.apps.ocp43prod.acic.local/dist/build-hash.json ;done
+*//' -e 's/  */./g')" http://XXXXX/dist/build-hash.json ;done
